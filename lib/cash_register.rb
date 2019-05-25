2027842aa770a2ea021513171959e9d00249f625
Chris Kay <chriskay25@gmail.com>
2019-05-25 05:10:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+  end 
+  
+  def total 
+    @total 
+  end 
+  
+  def discount
+    @discount
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+  
+  def apply_discount 
+    @total = @total.to_f * @discount.to_f
+  end 
+  
+  
+  
+  
+end 
+
+
+
+
+
+
 
